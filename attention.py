18a5eb3a7cb6b56193e3455cd68248e34f0a234a
e-b-daniel <ericbrdaniel@gmail.com>
2024-12-12 18:39:40 -0800

    implemented flash attention

--- attention.py
+++ attention.py
@@ -2,37 +2,59 @@
 Contains various implementations of scaled dot product attention.
 See section 3.2.1 of Vaswani et al., 2018.
 """
+import math
+import time
 import torch
 import torch.nn.functional as F
 from torch.nn.attention import SDPBackend, sdpa_kernel
-from flash import flash_attention_triton
+import triton
+import triton.language as tl
 
-def normal_attention_ours(q, k, v, mask=None):
-    """
-    Our naive implementation of scaled dot product attention in Python.
-    """
-    logits = q @ k.transpose(-2, -1)
-    if mask is not None:
-        logits = logits.masked_fill(mask == 0, float('-inf'))
-    scaled_logits = logits / (q.size(-1) ** 0.5)
-    scores = F.softmax(scaled_logits, dim=-1)
-    out = scores @ v
-    return out
-
-def normal_attention_torch(q, k, v, mask=None):
+def normal_attention_torch(q, k, v, is_causal=False):
     """
     Torch's implementation of scaled dot product attention in C++.
     """
     with sdpa_kernel(backends=SDPBackend.MATH):
-        return F.scaled_dot_product_attention(q, k, v, attn_mask=mask, dropout_p=0.0)
+        start = time.perf_counter()
+        output =  F.scaled_dot_product_attention(q, k, v, is_causal=is_causal, dropout_p=0.0)
+        end_time = time.perf_counter() - start
+        return output, end_time
+    # start = time.perf_counter()
+    # output = scaled_dot_product_attention(q, k, v, is_causal=is_causal)
+    # end_time = time.perf_counter() - start
+    # return output, end_time
+    
+    
 
-def flash_attention_ours(q, k, v, mask=None):
-    """
-    Our implementation of FlashAttention in Triton.
-    """
-    return flash_attention_triton(q, k, v, mask)
+def scaled_dot_product_attention(query, key, value, attn_mask=None,
+        is_causal=False, scale=None, enable_gqa=False) -> torch.Tensor:
+    L, S = query.size(-2), key.size(-2)
+    scale_factor = 1 / math.sqrt(query.size(-1)) if scale is None else scale
+    attn_bias = torch.zeros(L, S, dtype=query.dtype).to(query.device)
+    if is_causal:
+        assert attn_mask is None
+        temp_mask = torch.ones(L, S, dtype=torch.bool).tril(diagonal=0).to(query.device)
+        attn_bias.masked_fill_(temp_mask.logical_not(), float("-inf"))
+        attn_bias.to(query.dtype)
 
-def flash_attention_torch(q, k, v, mask=None):
+    if attn_mask is not None:
+        if attn_mask.dtype == torch.bool:
+            attn_bias.masked_fill_(attn_mask.logical_not(), float("-inf"))
+        else:
+            attn_bias += attn_mask
+
+    if enable_gqa:
+        key = key.repeat_interleave(query.size(-3)//key.size(-3), -3)
+        value = value.repeat_interleave(query.size(-3)//value.size(-3), -3)
+
+    attn_weight = query @ key.transpose(-2, -1) * scale_factor
+    attn_weight += attn_bias
+    attn_weight = torch.softmax(attn_weight, dim=-1)
+    return attn_weight @ value
+
+
+
+def flash_attention_torch(q, k, v, is_causal=False):
     """
     Torch's implementation of FlashAttention.
     """
@@ -43,7 +65,132 @@ def flash_attention_torch(q, k, v, mask=None):
         v = v.unsqueeze(0)
             
     with sdpa_kernel(backends=SDPBackend.FLASH_ATTENTION):
-        return F.scaled_dot_product_attention(q, k, v, attn_mask=mask, dropout_p=0.0)
+        start = time.perf_counter()
+        result = F.scaled_dot_product_attention(q, k, v, is_causal=is_causal, dropout_p=0.0) 
+        end_time = time.perf_counter() - start
+        return result, end_time
+# refrenced from https://github.com/openai/triton/blob/master/python/tutorials/06-fused-attention.py v1 changelog
+@triton.jit
+def _fwd_kernel(
+    Q,
+    K,
+    V,
+    sm_scale,
+    L, # output buffer for softmax denominators
+    M,  # output buffer for maxes
+    Out, # output 
+    # stride is for pointer arithmetic: 1 * stride gets you to the next element in the given axis (axis 0 is row, so have to go through full column if 2d)
+    stride_head,    # step to get to next head
+    stride_seq,     # step to get to next sequence within the head
+    hidden_dim,     # step to get to next hidden dim
+    seq_len,
+    Br: tl.constexpr,  # Br in paper
+    Bt: tl.constexpr,  # Bt in paper
+    hidden: tl.constexpr,  # Hidden dim
+    is_causal: tl.constexpr,
+):  
+    start_m = tl.program_id(0)      # which portion of the sequence am I in? Which block of the queries is mine?
+    off_hz = tl.program_id(1)       # which batch and head am I in? But really just which head?
+    # initialize offsets (we grab a specific query embedding block and then iterate through k and v's in blocks)
+    offs_br = start_m * Br + tl.arange(0, Br)      # get all of the rows of the query that I am responsible for
+    offs_bt = tl.arange(0, Bt)                          # Identify the columns that I am responsible for
+    offs_d = tl.arange(0, hidden)                     # get me all the hidden dims for the blocks
 
-def unspecified_attention_torch(q,k,v):
-    F.scaled_dot_product_attention(q, k, v)
+    off_q = off_hz * stride_head + offs_br[:, None] * stride_seq + offs_d[None, :] * hidden_dim
+    #             ^^^                           ^^^                           ^^^
+    #       get the right head    get all of the correct queries      get all of the hidden dims
+    # same logic for K and V but using the columns for the correct k,v
+    off_kv = off_hz * stride_head + offs_bt[:, None] * stride_seq + offs_d[None, :] * hidden_dim
+    # Initialize pointers to Q, K, V using the above offsets
+    q_ptrs = Q + off_q  
+    k_ptrs = K + off_kv
+    v_ptrs = V + off_kv
+    
+    # create buffers for m_i, l_i, and accumulator.
+    m_i = tl.zeros([Br], dtype=tl.float16) - float("inf") # maxes is negative infinity
+    l_i = tl.zeros([Br], dtype=tl.float16) # no denominators yet
+    acc = tl.zeros([Br, hidden], dtype=tl.float16) # zero out accumulator
+    
+    # load q: it will stay in SRAM throughout
+    q = tl.load(q_ptrs)
+    # loop over k, v and update accumulator
+
+    for start_n in tl.range(0, (start_m + 1) * Br, Bt):  # I think this is wrong
+    # for start_n in tl.range(0, seq_len, Bt):
+        # load k, v
+        k = tl.load(k_ptrs + start_n * stride_seq)
+        v = tl.load(v_ptrs + start_n * stride_seq)
+        # Calculate qk and mask out future if causal
+        qk = tl.dot(q, tl.trans(k)) * sm_scale
+        if is_causal:
+            qk += tl.where(offs_br[:, None] >= (start_n + offs_bt[None, :]), 0, float("-inf")) # mask out future
+        
+        # Compute the softmax using the old max
+        m_ij = tl.max(qk, 1)
+        p = tl.exp(qk - m_ij[:, None])
+
+        l_ij = tl.sum(p, 1)
+        
+        # update the max and denominator
+        m_i_new = tl.maximum(m_i, m_ij)
+        # alpha can be multiplied to cancel the old max, and then subtract the new max
+        alpha = tl.exp(m_i - m_i_new)
+        # beta can be multiplied to cancel the current max in q and then subtract the new max
+        beta = tl.exp(m_ij - m_i_new)
+        l_i_new = alpha * l_i + beta * l_ij
+        
+        # scale the softmax proabilities by new max and new denominator
+        p = p * (beta / l_i_new)[:, None]
+        p = p.to(v.dtype)
+
+        # scale the output attention values by the new max and new denominator
+        acc_scale_factor = l_i / l_i_new * alpha
+        acc = acc * acc_scale_factor[:, None] + tl.dot(p, v)
+        # update m_i and l_i to the new values
+        acc = acc.to(v.dtype) # make sure the dtype is correct
+        l_i = l_i_new.to(l_i.dtype)
+        m_i = m_i_new.to(m_i.dtype)
+    # write back l and m, and o
+    l_ptrs = L + off_hz * seq_len + offs_br
+    m_ptrs = M + off_hz * seq_len + offs_br
+    out_ptrs = Out + off_q
+    tl.store(l_ptrs, l_i)
+    tl.store(m_ptrs, m_i)
+    tl.store(out_ptrs, acc)
+
+def triton_forward(q, k, v, is_causal=False):
+    start = time.perf_counter()
+    BLOCK = 64
+    o = torch.empty_like(q)  # create space for output
+    sm_scale = q.size(-1) ** -0.5
+
+    # 2D launch grid of (r,c) where r = ceil(N/block size) and c = B*H
+    # So, grid = (seqlen per block, batch-heads)
+    # We are free to parallelize over any part of the query sequence (arbitrary query embedding attn output doesn't depend on other query embeddings)
+    # We are free to parallelize over batch and heads
+    grid = (triton.cdiv(q.shape[2], BLOCK), q.shape[0] * q.shape[1])
+    
+    # in paper, L and m are of size N; here, N-length L and m for each batch-head
+    L = torch.empty((q.shape[0] * q.shape[1], q.shape[2]), device=q.device, dtype=torch.float32)    # Softmax denominators
+    m = torch.empty((q.shape[0] * q.shape[1], q.shape[2]), device=q.device, dtype=torch.float32)    # maxes
+    
+    _fwd_kernel[grid](
+        q,
+        k,
+        v,
+        sm_scale,
+        L,
+        m,
+        o,
+        q.stride(1),
+        q.stride(2),
+        q.stride(3),
+        q.shape[2],
+        Br=BLOCK,
+        Bt=BLOCK,
+        hidden=q.shape[3],
+        is_causal=is_causal,
+    )
+    end_time = time.perf_counter() - start
+    return o, end_time
+
--- benchmark.py
+++ benchmark.py
@@ -1,55 +1,99 @@
+import time
 from typing import Callable
 import torch
-from timeit import timeit
-from functools import partial
 from attention import *
-from flash import *
 
-def benchmark(atten_fn: Callable, batch_size, seq_len, hidden_dim, causal_mask=False, n=10):
-    q = torch.rand(batch_size, seq_len, hidden_dim)[0]
-    k = torch.rand(batch_size, seq_len, hidden_dim)[0]
-    v = torch.rand(batch_size, seq_len, hidden_dim)[0]
-    print('trying')
-    flash_attention_python(q=q, k=k, v=v)
-    print('done')
-    # mask = torch.tril(torch.ones((seq_len, seq_len))) if causal_mask else None
-    # avg_time = timeit(lambda:atten_fn(q=q, k=k, v=v), number=n) / n
-    # return avg_time
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def check_accuracy(unverified_atten: Callable):
-    seq_len, hidden_dim = 64, 16
-    q = torch.rand(seq_len, hidden_dim)
-    k = torch.rand(seq_len, hidden_dim)
-    v = torch.rand(seq_len, hidden_dim)
-    print('t2')
-    ground_truth = normal_attention_torch(q,k,v)
-    print('t3')
-    test_implementation = unverified_atten(q,k,v)
-    print('t4')
+
+def benchmark(atten_fn: Callable, batch_size, num_heads, seq_len, hidden_dim, causal_mask=True, n=40):
+    avg_time = 0
+    for _ in range(n):
+        q = torch.rand(batch_size,num_heads, seq_len, hidden_dim, dtype=torch.float16, device=device, requires_grad=False)        
+        k = torch.rand(batch_size, num_heads,seq_len, hidden_dim, dtype=torch.float16,device=device, requires_grad=False)
+        v = torch.rand(batch_size, num_heads,seq_len, hidden_dim, dtype=torch.float16, device=device, requires_grad=False)
+        # warmup iteration
+        torch.cuda.empty_cache()
+        torch.cuda.synchronize()
+        atten_fn(q=q, k=k, v=v, is_causal=causal_mask)
+        torch.cuda.empty_cache()
+        torch.cuda.synchronize()
+        _, time_taken = atten_fn(q=q, k=k, v=v, is_causal=causal_mask)
+        avg_time += (time_taken) / n
+        del q, k, v
+    return avg_time
+
+def check_accuracy(unverified_atten: Callable, batch_size, num_heads, seq_len, hidden_dim, is_causal=True):
+    q = torch.rand(batch_size,num_heads, seq_len, hidden_dim, dtype=torch.float16, device=device, requires_grad=False)        
+    k = torch.rand(batch_size, num_heads,seq_len, hidden_dim, dtype=torch.float16,device=device, requires_grad=False)
+    v = torch.rand(batch_size, num_heads,seq_len, hidden_dim, dtype=torch.float16, device=device, requires_grad=False)
+
+    ground_truth, _ = normal_attention_torch(q,k,v, is_causal=is_causal)
+    test_implementation, _ = unverified_atten(q,k,v, is_causal=is_causal)
     if torch.allclose(ground_truth, test_implementation):
         print('Implementation is correct')
     else:
         print('Implementation is incorrect')
         print(torch.nn.functional.mse_loss(ground_truth, test_implementation))
         bools = torch.isclose(ground_truth, test_implementation)
-        percent_correct = torch.sum(bools) / sum(bools.size())
+        percent_correct = torch.sum(bools) / bools.numel() * 100
         print(f'Only {percent_correct}% of output values matched the ground truth implementation.')
-    # mask = torch.tril(torch.ones((seq_len, seq_len))) if causal_mask else None
-    # avg_time = timeit(lambda:atten_fn(q=q, k=k, v=v), number=n) / n
-    # return avg_time
-  
+
+@torch.no_grad()
 def main():
-    print('yo')
-    batch_size = 256
-    seq_len = 512
-    hidden_dim = 2048
-    check_accuracy(flash_attention_python)
-    # print()
-    # print("\tNormal Attention (Ours / Python): \t", benchmark(normal_attention_ours, batch_size, seq_len, hidden_dim))
-    # print("\tNormal Attention (Torch / C++):   \t", benchmark(normal_attention_torch, batch_size, seq_len, hidden_dim))
-    # print("Flash Attention (Ours / Triton): \t", benchmark(flash_attention_python, batch_size, seq_len, hidden_dim))
-    # print("\tFlash Attention (Torch / C++):   \t", benchmark(flash_attention_torch, batch_size, seq_len, hidden_dim))
-    # print()
+    batch_size = 50
+    seq_len = 1024
+    hidden_dim = 64
+    num_heads = 12
+
+    print("Warmup")
+    print("\tNormal Attention (Torch / C++):   \t", benchmark(normal_attention_torch, batch_size, num_heads, seq_len, hidden_dim,n=3))
+    print("\tFlash Attention (Ours / Triton): \t", benchmark(triton_forward, batch_size, num_heads, seq_len, hidden_dim, n=3))
+    print("\tFlash Attention (Torch / C++):   \t", benchmark(flash_attention_torch, batch_size,num_heads,  seq_len, hidden_dim, n=3))
+    print()
+    
+    # check all the accuracies
+    print("Checking Accuracy")
+    print("\tNormal Attention (Torch / C++):")
+    check_accuracy(normal_attention_torch, batch_size, num_heads, seq_len, hidden_dim)
+    print("\tFlash Attention (Ours / Triton):")
+    check_accuracy(triton_forward, batch_size, num_heads, seq_len, hidden_dim)
+    print("\tFlash Attention (Torch / C++):")
+    check_accuracy(flash_attention_torch, batch_size, num_heads, seq_len, hidden_dim)
+
+    print("Benchmark")
+    print("\tNormal Attention (Torch / C++):   \t", benchmark(normal_attention_torch, batch_size, num_heads, seq_len, hidden_dim))
+    print("\tFlash Attention (Ours / Triton): \t", benchmark(triton_forward, batch_size, num_heads, seq_len, hidden_dim))
+    print("\tFlash Attention (Torch / C++):   \t", benchmark(flash_attention_torch, batch_size, num_heads, seq_len, hidden_dim))
+    # return
+    # make plot
+    import matplotlib.pyplot as plt
+    import numpy as np
+    normal = []
+    flash = []
+    triton = []
+    batch_sizes = np.arange(9, 50, 1)
+    for batch_size in batch_sizes:
+        normal.append(benchmark(normal_attention_torch, batch_size, num_heads, seq_len, hidden_dim))
+    for batch_size in batch_sizes:
+        flash.append(benchmark(flash_attention_torch, batch_size, num_heads, seq_len, hidden_dim))
+    for batch_size in batch_sizes:
+        triton.append(benchmark(triton_forward, batch_size, num_heads, seq_len, hidden_dim))
+    batch_sizes = batch_sizes[1:]
+    flash = flash[1:]
+    triton = triton[1:]
+    normal = normal[1:]
+    # plot the speedup
+    flash_speedup = np.array(normal) / np.array(flash)
+    triton_speedup = np.array(normal) / np.array(triton)
+    plt.plot(batch_sizes, flash_speedup, label='Official Flash Speedup')
+    plt.plot(batch_sizes, triton_speedup, label='Our Triton Speedup')
+    plt.title('Speedup of Triton and Flash Attention')
+    plt.xlabel('Batch Size')
+    plt.ylabel('Speedup')
+    # plt.ylim(0, 100)
+    plt.legend()
+    plt.savefig('speedup.png')
 
 if __name__ == '__main__':
     main()
